5035ab661c595723d2b50df1d43cf13431073424
Beatriz <beatriz.belchior95@outlook.com>
2021-11-21 20:28:40 -0300

    iniciando o projeto. criado HTM, CSS e React.

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Black Slim Shirt',
+            category: 'Shirts',
+            image: '/images/all_black.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'hight quality product'
+        },
+        {
+            _id: '2',
+            name: 'Red Slim Shirt',
+            category: 'Shirts',
+            image: '/images/red.jpg',
+            price: 79,
+            brand: 'Adidas',
+            rating: 5,
+            numReviews: 2,
+            description: 'hight quality product'
+        },
+        {
+            _id: '3',
+            name: 'Black Slim Shirt',
+            category: 'Shirts',
+            image: '/images/all_black.jpg',
+            price: 99,
+            brand: 'Nike',
+            rating: 3,
+            numReviews: 15,
+            description: 'hight quality product'
+        },
+        {
+            _id: '4',
+            name: 'Red Slim Shirt',
+            category: 'Shirts',
+            image: '/images/red.jpg',
+            price: 130,
+            brand: 'Adidas',
+            rating: 4,
+            numReviews: 22,
+            description: 'hight quality product'
+        },
+        {
+            _id: '5',
+            name: 'Black Slim Shirt',
+            category: 'Shirts',
+            image: '/images/all_black.jpg',
+            price: 110,
+            brand: 'Nike',
+            rating: 5,
+            numReviews: 1,
+            description: 'hight quality product'
+        },
+        {
+            _id: '6',
+            name: 'Red Slim Shirt',
+            category: 'Shirts',
+            image: '/images/red.jpg',
+            price: 89,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 6,
+            description: 'hight quality product'
+        },
+    ],
+};
+
+export default data;
